e75b8b64b4b5b6471bd648dd2b022711c9a56126
Sami Hafid <sahafid@e2r6p2.1337.ma>
2022-04-21 00:25:15 +0000

    maintain

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 let i = 0;
 let count_fayrouz = 0;
 let count_jetske = 0;
@@ -8,6 +6,7 @@ let count_oscar = 0;
 let count_smokey = 0;
 let count_Ginger = 0;
 
+let depends = 0;
 
 
 const fayrouz_image = "<img src='cat.jpeg' alt='cat'>";
@@ -16,7 +15,6 @@ const patch_image = "<img src='cat3.jpeg' alt='cat'>";
 const oscar_image = "<img src='cat4.jpeg' alt='cat'>";
 const smokey_image = "<img src='cat5.jpeg' alt='cat'>";
 const Ginger_image = "<img src='cat6.jpeg' alt='cat'>";
-
 
 const fayrouz = document.getElementById('fayrouz');
 const jetske = document.getElementById('jetske');
@@ -33,46 +31,71 @@ let put = e => {
 	count_cat.innerHTML = e;
 };
 
-
 image.addEventListener('click', function (){
-	count_fayrouz++;
-	put(count_fayrouz);
+	if (depends == 1)
+	{
+		count_fayrouz++;
+		put(count_fayrouz);
+	}
+	else if (depends == 2)
+	{
+		count_jetske++;
+		put(count_jetske);
+	}
+	else if (depends == 3)
+	{
+		count_patche++;
+		put(count_patche);
+	}
+	else if (depends == 4)
+	{
+		count_oscar++;
+		put(count_oscar);
+	}
+	else if (depends == 5)
+	{
+		count_smokey++;
+		put(count_smokey);
+	}
+	else if (depends == 6)
+	{
+		count_Ginger++;
+		put(count_Ginger);
+	}
 });
 
-
 fayrouz.addEventListener('click', function (){
-	count_fayrouz++;
-	image.innerHTML = fayrouz_image;
 	count_cat.innerHTML = count_fayrouz;
+	depends = 1;
+	image.innerHTML = fayrouz_image;
 });
 
 jetske.addEventListener('click', function (){
-	count_jetske++;
+	count_cat.innerHTML = count_jetske;
+	depends = 2;	
 	image.innerHTML = jetske_image;
-	count_cat.innerHTML = count_fayrouz;
 });
 
 patch.addEventListener('click', function (){
-	count_patche++;
+	count_cat.innerHTML = count_patche;
+	depends = 3;
 	image.innerHTML = patch_image;
-	count_cat.innerHTML = count_fayrouz;
 });
 
 oscar.addEventListener('click', function (){
-	count_oscar++;
+	count_cat.innerHTML = count_oscar;
+	depends = 4;
 	image.innerHTML = oscar_image;
-	put(count_oscar);
 });
 
 smokey.addEventListener('click', function (){
-	count_smokey++;
+	count_cat.innerHTML = count_smokey;
+	depends = 5;
 	image.innerHTML = smokey_image;
-	put(count_smokey);
 });
 
 Ginger.addEventListener('click', function (){
-	count_Ginger++;
+	count_cat.innerHTML = count_Ginger;
+	depends = 6;
 	image.innerHTML = Ginger_image;
-	put(count_fayrouz);
 });
-
